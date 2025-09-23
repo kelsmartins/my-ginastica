f9df89b6786bec65acedf5665ae24d5fc95e444f
klss <kelsoglauber11@gmail.com>
2025-09-23 07:34:17 -0400

    login base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,71 @@
-import Image from "next/image";
+'use client'
+
+import { use, useState } from "react"
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPass, setLoginPass] = useState('');
+
+  const [signupEmail, setSignupEmail] = useState('');
+  const [signupPass, setSignupPass] = useState('');
+
+  const [fullName, setFullName] = useState('');
+
+  return (
+    <div className="w-screen h-screen bg-blue-300 flex justify-center items-center flex-col">
+      <div className="w-[400px] h-[700px] bg-neutral-100 flex flex-col items-center rounded-md shadow-lg">
+        <div className="w-[100%] h-[125px] flex justify-center items-center flex-col">
+          <span className="w-[50px] h-[50px] rounded-xl bg-blue-300 mb-2"></span>
+          <h1 className="text-3xl font-bold text-black mt-2">Viver Ginástica</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="w-[100%] h-[200px] flex justify-center items-center flex-col">
+          <input type="email" 
+          className="h-[40px] w-[300px] text-black mb-4 bg-neutral-200 p-1 rounded-md" placeholder="Digite seu email"
+          value={loginEmail}
+          onChange={e => setLoginEmail(e.target.value)}/>
+          <input type="password" 
+          className="h-[40px] w-[300px] text-black mb-4 bg-neutral-200 p-1 rounded-md" placeholder="Confirme sua senha"
+          value={loginPass}
+          onChange={e => setLoginPass(e.target.value)}/>
+          <span className=""></span>
+          <p
+          className="text-black text-start">Esqueceu a senha? <a href="http://">Redefina aqui!</a></p>
+          <input type="button" 
+          value="entrar" 
+          className="w-[300px] h-[40px] bg-purple-400 rounded-md font-bold"/>
+        </div>
+
+        <div className="w-[100%] h-[350px] flex justify-center items-center flex-col">
+  <input
+    type="text"
+    className="h-[40px] w-[300px] text-black mb-4 bg-neutral-200 p-1 rounded-md"
+    placeholder="Digite seu nome completo"
+    value={fullName}
+    onChange={e => setFullName(e.target.value)}
+  />
+  <input
+    type="email"
+    className="h-[40px] w-[300px] text-black mb-4 bg-neutral-200 p-1 rounded-md"
+    placeholder="Digite seu email"
+    value={signupEmail}
+    onChange={e => setSignupEmail(e.target.value)}
+  />
+  <input
+    type="password"
+    className="h-[40px] w-[300px] text-black mb-4 bg-neutral-200 p-1 rounded-md"
+    placeholder="Digite sua senha"
+    value={signupPass}
+    onChange={e => setSignupPass(e.target.value)}
+  />
+  <input
+    type="button"
+    value="cadastrar"
+    className="w-[300px] h-[40px] bg-purple-400 rounded-md font-bold"
+  />
+</div>
+
+      </div>
     </div>
-  );
+  )
 }
